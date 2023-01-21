aae354cb68ef00f8a36a5c705409bd8bcd1640cf
Joel John <joeljohn5112@gmail.com>
2023-01-21 13:58:56 +0530

    New Folder added

---
+++ codechef/1 star to 2 star/ARRSWAP.py
@@ -0,0 +1,8 @@
+t = int(input(''))
+while(t>0):
+    n = int(input(''))
+    a = list()
+    b = list()
+    a[n] = [int(x) for x in input("").split()]
+    b[n] = [int(x) for x in input("").split()]
+    t = t-1
